d757c90299dd456f575f7371f2af5790b6bc3630
Jay Shong <jay0shong@gmail.com>
2019-04-05 21:56:57 -0400

    set up initial redux for dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">main app component</div>;
+    return (
+      <Provider store={store}>
+        {" "}
+        <div className="App">main app component</div>{" "}
+      </Provider>
+    );
   }
 }
 
