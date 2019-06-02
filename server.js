9c8782b9045281c84118b7be2331ad9d2fd036a3
Jay Shong <jay0shong@gmail.com>
2019-06-02 18:39:35 -0400

    testing

--- server.js
+++ server.js
@@ -89,13 +89,8 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   }); */
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-} else {
-  app.use(express.static(path.join(__dirname, "/client/public")));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
   });
 }
 
