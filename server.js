1a3b5a7f5d9d2ef2e2bbdbe486a453e83433ed22
Jay Shong <jay0shong@gmail.com>
2019-06-02 18:47:11 -0400

    testing

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   }); */
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "/client/build/index.html"));
   });
 }
