1f127d058cff1c8c1e4abbcb38961738dcfed2a9
Jay Shong <jay0shong@gmail.com>
2019-06-02 16:25:44 -0400

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { Container } from "reactstrap";
 import { Provider } from "react-redux";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store";
 import history from "./history/History";
 import jwt_decode from "jwt-decode";
