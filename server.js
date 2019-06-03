b85c4ec4e0e264000a7860c52e8bbcd28c60c4d8
Jay Shong <jay0shong@gmail.com>
2019-06-02 20:45:00 -0400

    testing

--- server.js
+++ server.js
@@ -74,8 +74,6 @@ app.use(bodyParser.json());
 app.use(busboy());
 app.use(busboyBodyParser());
 
-app.use(express.static("client/build", { maxAge: "7d" }));
-
 app.use("/team", teamApi);
 app.use("/team/appointment", appointmentApiTeam);
 app.use("/user", userApi);
@@ -84,21 +82,12 @@ app.use("/confirm", confirm);
 app.use("/reset/user", userReset);
 app.use("/reset/team", teamReset);
 app.use("/daysoff", daysOff);
-/*
+
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
   app.use(express.static("client/build", { maxAge: "7d" }));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  }); 
-} */
-
-app.get("/forgot", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
-});
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
-});
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`server connected on ${port}`));
